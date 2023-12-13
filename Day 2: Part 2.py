ea8b73512c330b393d182a016b9688d039df0b05
raysiuwon <107330858+raysiuwon@users.noreply.github.com>
2023-12-12 23:34:31 -0500

    Update Day 2: Part 2.py

--- Day 2: Part 2.py
+++ Day 2: Part 2.py
@@ -8,9 +8,9 @@ with open(path,'r') as file:
 sumID = 0
 
 for l in range(len(line)):
-    red = max(list(map(int,re.findall('([0-9]|[1-9][0-9]) red',line[l]))))
-    green = max(list(map(int,re.findall('([0-9]|[1-9][0-9]) green',line[l]))))
-    blue = max(list(map(int,re.findall('([0-9]|[1-9][0-9]) blue',line[l]))))
+    red = max(list(map(int,re.findall('(\d+) red',line[l]))))
+    green = max(list(map(int,re.findall('(\d+) green',line[l]))))
+    blue = max(list(map(int,re.findall('(\d+) blue',line[l]))))
     cubed = red*green*blue
     sumID += cubed
 
