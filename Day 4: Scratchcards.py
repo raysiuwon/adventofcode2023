c8f078452d88a2f28fe80585d85c5fe5cdf09474
raysiuwon <107330858+raysiuwon@users.noreply.github.com>
2023-12-15 20:43:20 -0500

    Create Day 4: Scratchcards.py

---
+++ Day 4: Scratchcards.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+path = '/home/ray/Documents/programming/python/coding problems/advent of code/day4_input.txt'
+cards = open(path).read().splitlines()
+
+points = 0
+
+from collections import Counter
+for card in cards:
+    c = card.split(':')[1:]
+    count_cards = c[0].replace('|','')
+    cc = count_cards.split()
+    cc_points = Counter(cc)
+    total = [cc_points[k] for k in cc_points]
+    if total.count(2) >= 1:
+        points += (1*2**(total.count(2)-1))
+
+print(points)
