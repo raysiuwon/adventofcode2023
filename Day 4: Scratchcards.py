cac1b76c089b69e512407db9784c488890da974d
raysiuwon <107330858+raysiuwon@users.noreply.github.com>
2023-12-15 20:46:31 -0500

    Update Day 4: Scratchcards.py

--- Day 4: Scratchcards.py
+++ Day 4: Scratchcards.py
@@ -7,7 +7,7 @@ points = 0
 
 from collections import Counter
 for card in cards:
-    c = card.split(':')[1:]
+    c = card.split(':')[1]
     count_cards = c[0].replace('|','')
     cc = count_cards.split()
     cc_points = Counter(cc)
